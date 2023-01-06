ced310c16c238a6375d2604fe98415a48b5daad3
David M. Chen <chen7david@me.com>
2023-01-06 08:08:35 +0800

    initialized project

---
+++ lib/aws-cdk-services-playground-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AwsCdkServicesPlaygroundStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'AwsCdkServicesPlaygroundQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
